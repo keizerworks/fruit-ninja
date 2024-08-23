531fbd39b72ee042f00744cc5bc3d5e895f259f9
AryanBhati7 <codiearyan07@gmail.com>
2024-08-23 21:17:06 +0530

    zoom-out-bug-fixed

--- js/singleplayer.js
+++ js/singleplayer.js
@@ -1,6 +1,7 @@
 // js/singleplayer.js
 function startSingleplayer() {
   document.getElementById("singleplayerBtn").style.display = "none";
+  document.getElementById("multiplayerBtn").style.display = "none";
   isPlay = true;
   start.play();
   loop();
