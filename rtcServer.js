37db3fc004ad21c5635b09be600c87e3db345ac8
Sahil-Gulihar <sahilgullihar@.com>
2024-08-28 16:41:46 +0530

    [Feature]: RTC Support

---
+++ rtcServer.js
@@ -0,0 +1,17 @@
+// signaling-server.js
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    // Broadcast the message to all other clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+console.log('Signaling server running on ws://localhost:8080');
